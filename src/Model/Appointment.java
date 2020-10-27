caa16e7850e140ea51f59dc0fd964a8bbc9ca533
Roger Le <rogermle@gmail.com>
2020-10-27 00:31:12 -0700

    Reorganized Appointment model

--- src/Model/Appointment.java
+++ src/Model/Appointment.java
@@ -63,139 +63,147 @@ public class Appointment {
         this.customerName = customerName;
     }
 
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
     public void setAppointmentId(int appointmentId) {
         this.appointmentId = appointmentId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
     }
 
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
+    public int getUserId() {
+        return userId;
+    }
+
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
     }
 
     public void setLocation(String location) {
         this.location = location;
     }
 
+    public String getContact() {
+        return contact;
+    }
+
     public void setContact(String contact) {
         this.contact = contact;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public void setType(String type) {
         this.type = type;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getStart() {
+        return start;
     }
 
     public void setStart(String start) {
         this.start = start;
     }
 
+    public String getEnd() {
+        return end;
+    }
+
     public void setEnd(String end) {
         this.end = end;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
     }
 
     public void setCreateDate(Date createDate) {
         this.createDate = createDate;
     }
 
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
     }
 
     public void setLastUpdate(Timestamp lastUpdate) {
         this.lastUpdate = lastUpdate;
     }
 
+    public String getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
     public void setLastUpdateBy(String lastUpdateBy) {
         this.lastUpdateBy = lastUpdateBy;
+    }
+
+    public String getCustomerName() {
+        return customerName;
     }
 
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
     public void setCustomer(Customer customer) {
         this.customer = customer;
-    }
-
-    public int getAppointmentId(){
-        return this.appointmentId;
-    }
-
-    public int getCustomerId(){
-        return this.customerId;
-    }
-
-    public int getUserId(){
-        return this.userId;
-    }
-
-    public String getTitle(){
-        return this.title;
-    }
-
-    public String getDescription(){
-        return this.description;
-    }
-
-    public String getLocation(){
-        return this.location;
-    }
-
-    public String getContact(){
-        return this.contact;
-    }
-
-    public String getType(){
-        return this.type;
-    }
-
-    public String getUrl(){
-        return this.url;
-    }
-
-    public String getStart(){
-        return this.start;
-    }
-
-    public String getEnd(){
-        return this.end;
-    }
-
-    public Date getCreateDate(){
-        return this.createDate;
-    }
-
-    public String getCreatedBy(){
-        return this.createdBy;
-    }
-
-    public Timestamp getLastUpdate(){
-        return this.lastUpdate;
-    }
-
-    public String getLastUpdateBy(){
-        return this.lastUpdateBy;
-    }
-
-    public String getCustomerName(){
-        return this.customerName;
     }
 }
