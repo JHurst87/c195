3dd030169638ad165e68e5daa822a143ecafa1e2
Roger Le <rogermle@gmail.com>
2020-10-27 00:29:16 -0700

    Address getters/setters

--- src/Model/Address.java
+++ src/Model/Address.java
@@ -1,4 +1,97 @@
 package Model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Address {
+    private int addressId;
+    private String address;
+    private String address2;
+    private int cityId;
+    private String postalCode;
+    private String phone;
+    private LocalDate createDate;
+    private String createdBy;
+    private LocalTime lastUpdate;
+    private String lastUpdateBy;
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
+    public void setLastUpdateBy(String lastUpdateBy) {
+        this.lastUpdateBy = lastUpdateBy;
+    }
 }
